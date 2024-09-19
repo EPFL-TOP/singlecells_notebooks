55e37be9b9da5d9e70b7c9210425d44f08bbff21
Clement Helsens <clementhelsens>
2024-09-19 10:24:36 +0200

    bug fix running nested loop

--- utils/utils_preview.py
+++ utils/utils_preview.py
@@ -96,8 +96,6 @@ def get_timelaps(file):
 
     for pos in range(num_pos):
         time_data[pos]=[timesteps[num_pos*frame+pos] for frame in range(num_frames)]
-        for frame in range(num_frames):
-            print('--- ',num_pos*frame+pos)
 
 
 def preprocess_image_pytorch(image_array):
@@ -110,8 +108,6 @@ def process(file, low_crop, high_crop, model_detect, n=-9999):
     device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
     get_timelaps(file)
 
-
-
     current_file=os.path.join(file)
     time_lapse_path = Path(current_file)
     print('time_lapse_path = ',time_lapse_path)
@@ -119,7 +115,6 @@ def process(file, low_crop, high_crop, model_detect, n=-9999):
     time_lapse = time_lapse.transpose(1,0,2,3,4)
 
     print(time_lapse.shape)#(81=t, 110=pos, 3, 512, 512)
-
 
     for pos_id, pos in enumerate(time_lapse):
         if n>0 and n==pos_id:break
